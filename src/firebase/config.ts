1230d64dc33202f4eceddf7b74c58dbd426b9d52
gabrielcoutinh0 <gabrielcoutinhof15@gmail.com>
2023-01-10 11:48:59 -0300

    adding google fonts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,6 +2,12 @@ import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
+  apiKey: "AIzaSyCcdHpg9fggzfNRus8mB1MDzFE96i_zDfQ",
+  authDomain: "miniblog-519fe.firebaseapp.com",
+  projectId: "miniblog-519fe",
+  storageBucket: "miniblog-519fe.appspot.com",
+  messagingSenderId: "857520941763",
+  appId: "1:857520941763:web:4aefc9b66b025990da1076",
   //PASTE YOUR FIREBASE SETTINGS HERE
 };
 
